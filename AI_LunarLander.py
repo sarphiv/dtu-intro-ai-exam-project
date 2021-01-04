42cb0e0fe9a6f75b92a429086b5aef3f0f2ab4ce
davidAlvaBro <davidalvabro@gmail.com>
2021-01-04 16:10:21 +0100

    i don't kniw what to do!

--- AI_LunarLander.py
+++ AI_LunarLander.py
@@ -1,6 +1,6 @@
 # Lunar Lander: AI-controlled play
 
-from ai_train import train
+from ai_train import normalize_state, train
 from LunarLander import *
 
 env = LunarLander()
@@ -14,28 +14,43 @@ import torch
 import torch.nn
 import random as r
 import numpy as np
-N = 64
+
+action_rate = 4
+
 input_neurons = 5
-hidden_neurons = 6
+hidden_neurons1 = 64
+hidden_neurons2 = 64
+#hidden_neurons3 = 64
 output_neurons = 8
 
+platform_center_x = 400
+
 #Distance, speed, fuel, win
-reward_factors = np.array([-1.0, -0.6, 0.1, 100])
+#reward_factors = np.array([-2.0, -0.2, 0.1, 100])
+reward_factors = np.array([-100, 0, 0, 100])
 
 
-# Creating network : 
+# Creating network :
 model = torch.nn.Sequential(
-            torch.nn.Linear(input_neurons, hidden_neurons),
-            torch.nn.Sigmoid(),
-            torch.nn.Linear(hidden_neurons, output_neurons),
-            # torch.nn.Softmax(),
-            torch.nn.Sigmoid(),
+            torch.nn.Linear(input_neurons, hidden_neurons1),
+            torch.nn.ReLU(),
+            torch.nn.Linear(hidden_neurons1, hidden_neurons2),
+            torch.nn.ReLU(),
+            torch.nn.Linear(hidden_neurons2, output_neurons),
+            torch.nn.Softmax(),
+
+            #torch.nn.Sigmoid(),
         )
 
 # Saving states 
+#[ [x, y, x_s, y_s, f], ... ]: List[List[float, float, float, float, float]]
 states = []
+#[ a, ... ]: List[int]
 actions = []
+#[ r, ... ]: List[float]
 rewards = []
+#[ t, ... ]: List[int] (list of terminal state indexes)
+terminals = [ ]
 
 
 def action_to_index(boost, left, right):
@@ -44,53 +59,74 @@ def action_to_index(boost, left, right):
 def index_to_action(index):
     return (index // 4, (index % 4) // 2, index % 2)
 
-
+counter = 0
+game_counter = 0
 while not exit_program:
+    counter += 1
+    
     if render_game:
         env.render()
-
-    #Get state vector
-    current_state = [env.rocket.x, env.rocket.y, 
-                     env.rocket.xspeed, env.rocket.yspeed, 
-                     env.rocket.fuel]
-    input = torch.tensor(current_state)
+        
+        
+    #TODO: Can we normalize inputs?
+    if counter % action_rate == 0:
+        #Get state
+        current_state = normalize_state(env.rocket.x, env.rocket.y, 
+                                        env.rocket.xspeed, env.rocket.yspeed, 
+                                        env.rocket.fuel)
+        input = torch.tensor(current_state)
     
-    #Forward-pass through model
-    # action_probabilities = model(input).detach().numpy()
-    action_probabilities = model(input).detach().numpy()
-    action_probabilities = action_probabilities / action_probabilities.sum()
+        #Forward-pass through model
+        # action_probabilities = model(input).detach().numpy()
+        action_probabilities = model(input).detach().numpy()
+        # action_probabilities = action_probabilities / action_probabilities.sum()
 
-    #Randomly sample action from distribution
-    action_index = np.random.choice(np.arange(output_neurons), p = action_probabilities)
-    (boost, left, right) = index_to_action(action_index)
+        #Randomly sample action from distribution
+        action_index = np.random.choice(np.arange(output_neurons), p = action_probabilities)
+        (boost, left, right) = index_to_action(action_index)
 
     #Step
     (x, y, x_speed, y_speed), reward, done = env.step((boost, left, right))
     fuel = env.rocket.fuel
-    
-    #Save state, action, reward
-    states.append(current_state)
-    actions.append([action_to_index(*(boost, left, right))])
+        
+        
+    if counter % action_rate == 0:
+        #Save state, action, reward
+        states.append(current_state)
+        actions.append([action_to_index(*(boost, left, right))])
+        
+        # Always append reward (no non-terminal rewards)
+        reward = 0
+        
+        # If the game is over we store the terminal rewards 
+        if done:
+            game_counter += 1
+            won = env.won
+            pos_delta = abs(x - platform_center_x)
+            speed = (x_speed**2 + y_speed**2)**0.5
 
-    if done:
-        won = env.won
-        pos_delta = ((x-400)**2 + (y-575)**2)**0.5
-        speed = (x_speed**2 + y_speed**2)**0.5
-
-        reward = (reward_factors * np.array([pos_delta, speed, fuel, won])).sum()
-
+            reward = (reward_factors * np.array([pos_delta, speed, fuel, won])).sum()
+            # Adds the index of the game 
+            terminals.append(len(rewards))
+            
         rewards.append(reward)
         
+        # When done train the network and clear lists  
+        if done:
+            if game_counter % 50 == 0:
+                train(model, states, actions, rewards, terminals)
+                states.clear()
+                actions.clear()
+                rewards.clear()
+                terminals.clear()
         
-        train(model, states, actions, rewards)
-        
-        rewards.clear()
-        states.clear()
-        actions.clear()
-        env.reset()
-        
-        
-  
+            env.reset()
+            env.rocket.x = 0
+            env.rocket.y = 300
+            env.rocket.xspeed = 0
+            env.rocket.yspeed = 0
+
+    
     # Process game events
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
--- ai_train.py
+++ ai_train.py
@@ -1,33 +1,66 @@
 
+import numpy as np
 import torch
+from typing import List
 
 
 # batch_size = 64
-learning_rate = 1e-4
+learning_rate = 1e-5
+future_discount = 0.99
 
 
-def train(model, states, actions, rewards):
-    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+def normalize_state(x, y, x_speed, y_speed, fuel):
+    return [x / 800, y / 600, x_speed / 60, y_speed / 60, fuel / 100]
+
+def standardize_rewards(rewards: np.ndarray):
+    std = rewards.std(ddof=1)
+    return (rewards - rewards.mean()) / (std if std else 1)
+
+
+def calculate_expected_reward(rewards: List[int], terminals: List[int]) -> np.ndarray:
+    padded_terminals = [-1] + terminals
+    expected_rewards = []
     
+    # Runs through every game and maps the expected reward 
+    for prev_t, curr_t in enumerate(padded_terminals[1:]):
+        trajectory_rewards = rewards[padded_terminals[prev_t]+1:curr_t+1]
+        total_expected_reward = sum(trajectory_rewards)
+
+        discount = future_discount
+        for r in trajectory_rewards:
+            expected_rewards.append(total_expected_reward * discount)
+            discount *= future_discount
+            total_expected_reward -= r
+
+    #Return stantdardized expected rewards
+    return standardize_rewards(np.array(expected_rewards))
+    #return np.array(expected_rewards)
+    
+
+def train(model, states, actions, rewards, terminals):
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+    loss = torch.nn.NLLLoss()
+
 
     t_states = torch.tensor(states)
     t_action = torch.tensor(actions)
-    
-    #TODO: WRITE ME TO BE GENERAL FOR MANY TRAJECTORIES
-    expected_reward = torch.tensor(rewards[0])
+    t_expected_rewards = torch.tensor(calculate_expected_reward(rewards, terminals))
     
     
     logits = model(t_states)
-    action_probs = logits / logits.sum(axis=1).reshape((-1, 1))
+    #action_probs = logits / logits.sum(axis=1).reshape((-1, 1))
+    action_probs = logits
 
     new_list = torch.zeros_like(action_probs, dtype=torch.bool)
     for i, c in enumerate(actions):
         new_list[i][c] = True
         
+    # probs = action_probs[new_list].log()
     probs = action_probs[new_list].log()
 
-    #TODO: Divide by number of games
-    negated_reward = -(probs * expected_reward).sum() / 1
+    
+    negated_reward = loss(logits.log(), torch.tensor([a[0] for a in actions], dtype=torch.long))
+    #negated_reward = -(probs * t_expected_rewards).mean()
     
     optimizer.zero_grad()
     negated_reward.backward()
